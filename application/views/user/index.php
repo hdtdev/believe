1c71cc5a8030f67a7ad8e87687ddf3ccddfab07e
ariefhdt <blackconnor78@gmail.com>
2020-06-01 14:38:28 +0700

    memperbaiki tampilan list di user

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -32,7 +32,7 @@
                 <div class="card shadow mb-4">
                         <!-- Card Header - Dropdown -->
                         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                          <span class="m-0 font-weight-bold"><?= $diary['name']?> </span><span style="width: 100%!important; text-align: right!important; padding-right: 4%"><?= $diary['tanggal_diary']?></span>
+                          <span class="m-0 font-weight-bold" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width: 80px"><?= $diary['name']?> </span><span style="width: 80%!important; text-align: right!important;"><?= date("d F Y", strtotime($diary['tanggal_diary'])) ?></span>
                           <div class="dropdown no-arrow">
                             <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
@@ -45,27 +45,24 @@
                         </div>
                         <!-- Card Body -->
                         <div class="card-body text-justify">
-                            <?php if($diary['only_psikolog'] == 1){ echo "<span>Dapat dilihat : Psikolog</span> | ";}else{echo "<span>Dapat dilihat : Publik</span> * ";}?>
+                            <?php if($diary['only_psikolog'] == 1){ echo "<i class='fa fa-user-md' ></i> | ";}else{echo "<i class='fa fa-globe'></i> | ";}?>
                             <?php 
                             if($diary['id_status'] == 1)
                                 { 
                                     //<span>Status post : Draft;</span>
-                                    echo "<span>Status post : Draft</span>";
+                                    echo "<i class='fa fa-clock-o'></i>";
                                 }else{
                                     //<span>Status post : Published</span> 
-                                    echo "<span>Status post : Published</span> ";
+                                    echo "<i class='fa fa-check-circle' ></i> ";
                                 }
                             ?>
                             <br>
 
-                            <p style="margin-bottom: 0!important; margin-top: 1%"><?= $diary['konten_diary']?></p> 
+                            <p style="margin-bottom: 0!important; margin-top: 1%; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; "><?= $diary['konten_diary']?></p> 
 
                             <br>
 
                             <a href="<?= site_url('diary/lihat/'.$diary['id_diary'])?>" style="padding-left: 0!important;" class="btn btn-secondary btn-icon-split">
-                                <span class="icon text-white-50">
-                                    <i class="fas fa-arrow-right"></i>
-                                </span>
                                 <span class="text btn-believe">Selengkapnya</span>
                             </a>
                         </div>
