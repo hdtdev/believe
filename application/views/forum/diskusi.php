36d86b2136b19bb43bfcc9a787224f1f5516c33d
ariefhdt <blackconnor78@gmail.com>
2020-06-05 20:16:53 +0700

    memperbarui tampilan chat forum (OK)

--- application/views/forum/diskusi.php
+++ application/views/forum/diskusi.php
@@ -7,7 +7,7 @@
     </div>
 
     <!-- here -->
-    <div class="container bg-gray-200" style="margin-bottom: 5%">
+    <div class="container bg-gray-200" style="margin-bottom: 3%">
         <div class="row text-center">
             <div class="col-sm-12 text-center" style="padding: 2%">
                 <p>Selamat datang di Forum diskusi tentang <strong><?= $diskusi['nama_list_forum']?></strong>. <?= $diskusi['deskripsi_list_forum']?></p>
@@ -16,19 +16,35 @@
     </div>
 
     <?php foreach($komentar as $komen):?>
-    <div class="container shadow" style="margin-top: 1%">
-        <div class="row" style="padding: 1%">
-            <div class="col-8">
-                <strong><?= $komen['name']?></strong>
-            </div>
-            <div class="col-4 text-right">
-                <i style="font-size: 14px"><?= date("d F Y", strtotime($komen['waktu_komentar_forum']))?></i>
-            </div>
-            <div class="col-sm-12">
-                <p><?= $komen['konten_komentar']?></p>
+
+    <?php if ($this->session->userdata('id')== $komen['id_user']) { ?>
+        <div class="bg-believe" style="margin-top: 1%; border-radius: 15px 0px 15px 15px; color: white" class="col-sm-12 shadow">
+            <div class="row" style="padding: 5px;">
+
+                <div class="col-12 text-right">
+                    <?= "<span style='font-size:14px'>".date("d F Y", strtotime($komen['waktu_komentar_forum'])) ."</span> <strong style='margin-left:10px'> ". $komen['name'] ."</strong>"?>
+                </div>
+                <div class="col-sm-12 text-left" style="padding-left: 25px; padding-right: 25px">
+                    <p><?= $komen['konten_komentar']?></p>
+                </div>
             </div>
         </div>
-    </div>
+            
+        <?php }else{?>
+ 
+            <div style="margin-top: 1%; border-radius: 0px 15px 15px 15px; color: white" class="shadow bg-dark col-sm-12">
+                <div class="row" style="padding: 5px;">
+
+                    <div class="col-12">
+                        <?= "<strong style='margin-left:10px'>". $komen['name'] ."</strong> <span style='font-size:14px'>". date("d F Y", strtotime($komen['waktu_komentar_forum'])) ."</span>"?>
+                    </div>
+                    <div class="col-sm-12 text-left" style="padding-left: 25px; padding-right: 25px">
+                        <p><?= $komen['konten_komentar']?></p>
+                    </div>
+                </div>
+            </div>
+        <?php }?>
+
     <?php endforeach;?>
 
     <div class="container" style="margin-top: 2%">
@@ -44,7 +60,7 @@
 
                     <div class="form-group row justify-content-end">
                         <div class="col-sm-12">
-                            <button name="submit_komentar_forum" type="submit" class="btn btn-primary">Submit</button>
+                            <button name="submit_komentar_forum" type="submit" class="btn btn-believe">Submit</button>
                         </div>
                     </div>
                 </form>
