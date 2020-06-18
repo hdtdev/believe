325a04e09f92877f371701f34d8736776f101a18
ariefhdt <blackconnor78@gmail.com>
2020-06-18 13:26:22 +0700

    memperbaiki fitur register psikolog

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -58,7 +58,10 @@ class Auth extends CI_Controller
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password!</div>');
                     redirect('auth');
                 }
-            } else {
+            } elseif($user['role_id'] == 2 && $user['is_active'] == 0){
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Waiting to accepted by admin!</div>');
+                redirect('auth');
+            }else {
                 $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
                 redirect('auth');
             }
@@ -86,7 +89,7 @@ class Auth extends CI_Controller
         $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
         if ($this->form_validation->run() == false) {
-            $data['title'] = 'User Registration';
+            $data['title'] = 'Psikolog Registration';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
@@ -133,11 +136,11 @@ class Auth extends CI_Controller
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
             'is_unique' => 'This email has already registered!'
         ]);
-        // $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
-        //     'matches' => 'Password dont match!',
-        //     'min_length' => 'Password too short!'
-        // ]);
-        // $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
         $error = [
             'ijin_praktek'=>['error'=>''],
